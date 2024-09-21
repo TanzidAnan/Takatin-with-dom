4affabd46327f1518075207e3168948dfc29a9f8
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-22 03:52:50 +0600

    seat funcation onclick

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("Tanzid anan")
+const selecdetSeatEl =document.getElementById('selecdet-seat')
+
+function handleSelectSate(event){
+    console.log(event.innerText);
+    selecdetSeatEl.innerHTML =`<li class="text-base font-normal flex justify-between ">
+        <span>${event.innerText}</span>
+        <span>Econone</span>
+        <span>550</span>
+    </li>`
+}
