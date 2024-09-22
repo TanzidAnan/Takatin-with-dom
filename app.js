8facf7d44d8478fe74680449dd013d927684fd9a
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-22 10:14:39 +0600

    count

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const selecdetSeatEl =document.getElementById('selecdet-seat');
-const totleBookedEl =document.getElementById('totle-booked')
+const totleBookedEl =document.getElementById('totle-booked');
+const availableSeat =document.getElementById('available-seat')
 let selectedSeat= []
 function handleSelectSate(event){
     const targetElement = event.innerText; 
     //  targetElement.classList.add('bg-red-300');
     selectedSeat.push(event.innerText);
     totleBookedEl.innerText =selectedSeat.length;
+
+    const availableSeatValue =parseInt(availableSeat.innerText);
+    const availableSeatNumber =availableSeatValue-1;
+    availableSeat.innerText =availableSeatNumber
+
+
+
     selecdetSeatEl.innerHTML =`<li class="text-base font-normal flex justify-between ">
         <span>${targetElement}</span>
         <span>Econone</span>
