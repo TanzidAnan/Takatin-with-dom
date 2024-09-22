3c4d6b6006c2e394d26fad860fa3d1fbbf8a40b4
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-22 11:00:04 +0600

    booking

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-const selecdetSeatEl =document.getElementById('selecdet-seat');
-const totleBookedEl =document.getElementById('totle-booked');
-const availableSeat =document.getElementById('available-seat')
-let selectedSeat= []
-function handleSelectSate(event){
-    const targetElement = event.innerText; 
-    //  targetElement.classList.add('bg-red-300');
-    selectedSeat.push(event.innerText);
-    totleBookedEl.innerText =selectedSeat.length;
+const selecdetSeatEl = document.getElementById('selecdet-seat');
+const totleBookedEl = document.getElementById('totle-booked');
+const availableSeat = document.getElementById('available-seat');
+const totlePriceEl = document.getElementById('totle-price');
+const couponInput = document.getElementById('coupon-field')
+const couponButton = document.getElementById('coupon-btn')
+let selectedSeat = [];
+let totlePrice = 0
+function handleSelectSate(event) {
+    const value = event.innerText;
 
-    const availableSeatValue =parseInt(availableSeat.innerText);
-    const availableSeatNumber =availableSeatValue-1;
-    availableSeat.innerText =availableSeatNumber
+    if (selectedSeat.includes(value)) {
+        alert('Seat already Booking');
+        return;
+    }
+    else if(selectedSeat.length<4) {
+        const targetElement = event.innerText;
+        //  targetElement.classList.add('bg-red-300');
+        selectedSeat.push(event.innerText);
+        totleBookedEl.innerText = selectedSeat.length;
+
+        const availableSeatValue = parseInt(availableSeat.innerText);
+        const availableSeatNumber = availableSeatValue - 1;
+        availableSeat.innerText = availableSeatNumber
 
 
 
-    selecdetSeatEl.innerHTML =`<li class="text-base font-normal flex justify-between ">
+        selecdetSeatEl.innerHTML += `<li class="text-base font-normal flex justify-between ">
         <span>${targetElement}</span>
         <span>Econone</span>
         <span>550</span>
     </li>`
-    console.log(selectedSeat)
+        totlePrice += 550;
+        totlePriceEl.innerText = totlePrice.toFixed(2);
+
+        if (selectedSeat.length > 3) {
+            couponInput.removeAttribute('disabled')
+            couponButton.removeAttribute('disabled')
+        }
+    }
+    else{
+        alert('maximun seat select');
+        return;
+    }
+
 }
