4882314cd003165ce8161d33413ed66ea9e92ffb
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-22 10:01:02 +0600

    seat

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const selecdetSeatEl =document.getElementById('selecdet-seat')
-
+const selecdetSeatEl =document.getElementById('selecdet-seat');
+const totleBookedEl =document.getElementById('totle-booked')
+let selectedSeat= []
 function handleSelectSate(event){
-    console.log(event.innerText);
+    const targetElement = event.innerText; 
+    //  targetElement.classList.add('bg-red-300');
+    selectedSeat.push(event.innerText);
+    totleBookedEl.innerText =selectedSeat.length;
     selecdetSeatEl.innerHTML =`<li class="text-base font-normal flex justify-between ">
-        <span>${event.innerText}</span>
+        <span>${targetElement}</span>
         <span>Econone</span>
         <span>550</span>
     </li>`
+    console.log(selectedSeat)
 }
