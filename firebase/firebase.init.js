61417b6756ff5f1c6e1505ed73043502624f13ff
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-09 22:00:22 +0600

    firebase

---
+++ firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB1YPZmgD2hTuLrC_YHOLj27ibZwTJxudI",
+    authDomain: "takatin-with-dom-125.firebaseapp.com",
+    projectId: "takatin-with-dom-125",
+    storageBucket: "takatin-with-dom-125.firebasestorage.app",
+    messagingSenderId: "252621176014",
+    appId: "1:252621176014:web:dd3b909420330d8203249f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
